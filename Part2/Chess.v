d3c09c00db0ad97a7d11f2da23c87487702957d5
lkvo <likvo@mail.ustc.edu.cn>
2025-12-10 18:19:28 +0800

    breif Frame

--- Part2/Chess.v
+++ Part2/Chess.v
@@ -1,9 +1,38 @@
 module Chess(
 
 );
+/* 全局寄存器设置 */
+// 状态寄存器 MENU 菜单、 PLAY 游戏、 SETTLE 结算
+reg [1:0] state;
+localparam MENU = 2'b00;
+localparam PLAY = 2'b01;
+localparam SETTLE = 2'b10;
+
+// 游戏状态管理
+always @(posedge clk) begin
+    case 
+
+end
 
 
+// 选择器模块
+Selector selector(
+      
+);
 
+// 音效模块
+Music music(
 
+);
+
+// 键盘模块
+Keyboard keyboard(
+
+);
+
+// 渲染模块
+GFX gfx(
+
+);
 
 endmodule
---
+++ Part2/Play.v
@@ -0,0 +1,6 @@
+module Play(
+
+);
+
+
+endmodule
