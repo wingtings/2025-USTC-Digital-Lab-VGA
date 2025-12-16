19586f850ecb1cc086a6cecea87f598533c76006
lkvo <likvo@mail.ustc.edu.cn>
2025-12-16 20:42:42 +0800

    img_orig

---
+++ Part2/Over.v
@@ -0,0 +1,15 @@
+// 游戏结束逻辑判断模块, 接收 board_data, 输出 game_over 信号
+module Over(
+    input clkk,
+    input rstn,
+    input board_data [12*64-1:0],
+    output reg [1:0] game_over
+);
+
+
+
+
+
+
+
+endmodule
